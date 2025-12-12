62355cd7b72b95112b5dfe7041a73661004ea77c
EnriqeD <henrykszambelan.pv@gmail.com>
2025-12-12 16:10:04 +0100

    Dodanie komentarzy doxygen

--- main.cpp
+++ main.cpp
@@ -1,6 +1,23 @@
-﻿#include <iostream>
+﻿/**
+ * @file main.cpp
+ * @brief Plik główny (entry point) programu testującego algorytm Merge Sort.
+ * * Plik ten zawiera funkcję main, która demonstruje działanie klasy MergeSorter
+ * na dwóch różnych typach danych: liczbach całkowitych (int) oraz
+ * liczbach zmiennoprzecinkowych (double).
+ */
+
+#include <iostream>
 #include "MergeSorter.h" 
 
+ /**
+  * @brief Pomocnicza funkcja szablonowa do wyświetlania zawartości tablicy.
+  * * Wypisuje elementy tablicy na standardowe wyjście (std::cout), oddzielając
+  * je spacjami i kończąc nową linią.
+  * * @tparam T Typ danych w tablicy. Typ ten musi obsługiwać operator
+  * przesunięcia bitowego w lewo (<<) dla strumienia wyjściowego.
+  * @param array Wskaźnik do tablicy, która ma zostać wyświetlona.
+  * @param size Liczba elementów w tablicy.
+  */
 template <typename T>
 void printArray(T* array, int size) {
     for (int i = 0; i < size; i++) {
@@ -9,6 +26,16 @@ void printArray(T* array, int size) {
     std::cout << std::endl;
 }
 
+/**
+ * @brief Główna funkcja programu.
+ * * Realizuje następujący scenariusz testowy:
+ * 1. Tworzy i inicjalizuje tablicę liczb całkowitych (int).
+ * 2. Sortuje ją przy użyciu instancji MergeSorter<int>.
+ * 3. Tworzy i inicjalizuje tablicę liczb zmiennoprzecinkowych (double).
+ * 4. Sortuje ją przy użyciu instancji MergeSorter<double>.
+ * * Wyniki przed i po sortowaniu są wypisywane na konsolę w celu weryfikacji.
+ * * @return Zwraca 0, jeśli program zakończył się poprawnie.
+ */
 int main() {
     // --- Test INT ---
     std::cout << "--- Test dla typu INT ---" << std::endl;
@@ -18,6 +45,7 @@ int main() {
     std::cout << "Przed: ";
     printArray(tabInt, sizeInt);
 
+    // Utworzenie instancji i sortowanie
     MergeSorter<int> sorterInt;
     sorterInt.sort(tabInt, sizeInt);
 
@@ -33,6 +61,7 @@ int main() {
     std::cout << "Przed: ";
     printArray(tabDouble, sizeDouble);
 
+    // Utworzenie instancji i sortowanie
     MergeSorter<double> sorterDouble;
     sorterDouble.sort(tabDouble, sizeDouble);
 
